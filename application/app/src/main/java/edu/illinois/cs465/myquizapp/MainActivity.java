5c701157baa9f8c6e76172e90be5b147f06d92da
YenChen <b06902047@ntu.edu.tw>
2022-11-07 17:50:29 -0600

    Add Single island & Island detailed

---
+++ application/app/src/main/java/edu/illinois/cs465/myquizapp/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs465.myquizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
