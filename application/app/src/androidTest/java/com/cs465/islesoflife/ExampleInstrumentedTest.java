0a0638779c6c14977ddb645fb184f1e7c8a96b5e
YU-Anthony <zhang.yuanthony@gmail.com>
2022-11-11 16:43:34 -0600

    update package name

--- application/app/src/androidTest/java/com/cs465/islesoflife/ExampleInstrumentedTest.java
+++ application/app/src/androidTest/java/com/cs465/islesoflife/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("net.penguincoders.doit", appContext.getPackageName());
+        assertEquals("com.cs465.islesoflife", appContext.getPackageName());
     }
 }
